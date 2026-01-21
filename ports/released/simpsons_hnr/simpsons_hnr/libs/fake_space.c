86dd18fcc814c542c2b1754ce71056278fe0b7e2
Jeod <47716344+JeodC@users.noreply.github.com>
2026-01-21 18:49:20 -0500

    [SIMPSONS] Fakespace uses what is confirmed to be used
    Attempted to resolve autosave issues, without success. Revert to only using what is confirmed to be hooked. Manual saving works.

--- ports/released/simpsons_hnr/simpsons_hnr/libs/fake_space.c
+++ ports/released/simpsons_hnr/simpsons_hnr/libs/fake_space.c
@@ -2,178 +2,51 @@
 
 // Modern SD Cards, when large, will cause an integer overflow and make the game think the drive is full
 // This shim tricks the game into reading a drive with 512MB free space, more than enough for save files
+// Autosaves will still give an error, but manual saving works
 // Compile with gcc -fPIC -shared -o libfakespace.so fake_space.c -ldl on debian bullseye
 
 #define _GNU_SOURCE
 #include <dlfcn.h>
 #include <sys/statvfs.h>
 #include <sys/vfs.h>
-#include <sys/syscall.h>
 #include <unistd.h>
-#include <stdarg.h>
-#include <stddef.h>
 #include <stdio.h>
+#include <string.h>
 
-static int fakespace_hits = 0;
-
-__attribute__((constructor))
-static void fakespace_init(void)
-{
-    fprintf(stderr, "[fakespace] libfakespace loaded\n");
-}
-
-__attribute__((destructor))
-static void fakespace_fini(void)
-{
-    if (fakespace_hits == 0)
-        fprintf(stderr,
-            "[fakespace] WARNING: shim loaded but never used\n");
-    else
-        fprintf(stderr,
-            "[fakespace] shim used (%d interceptions)\n",
-            fakespace_hits);
-}
-
-int statvfs(const char *path, struct statvfs *buf)
-{
-    static int (*orig)(const char *, struct statvfs *) = NULL;
-    if (!orig)
-        orig = dlsym(RTLD_NEXT, "statvfs");
-
-    int ret = orig(path, buf);
-    if (ret == 0)
-    {
-        fakespace_hits++;
-        buf->f_blocks = 1000000;
-        buf->f_bavail = 500000;
-        buf->f_bfree  = 500000;
-        buf->f_frsize = 1024;
+static void apply_fake_stats(void *buf, int is_vfs) {
+    if (is_vfs) {
+        struct statvfs *v = (struct statvfs *)buf;
+        v->f_bsize = v->f_frsize = 1024;
+        v->f_blocks = 32768; 
+        v->f_bfree = v->f_bavail = 16384;
+    } else {
+        struct statfs *s = (struct statfs *)buf;
+        s->f_bsize = 1024;
+        s->f_blocks = 32768; 
+        s->f_bfree = s->f_bavail = 16384;
     }
-    return ret;
 }
 
-int statvfs64(const char *path, struct statvfs64 *buf)
-{
-    static int (*orig)(const char *, struct statvfs64 *) = NULL;
-    if (!orig)
-        orig = dlsym(RTLD_NEXT, "statvfs64");
-
-    int ret = orig(path, buf);
-    if (ret == 0)
-    {
-        fakespace_hits++;
-        buf->f_blocks = 1000000;
-        buf->f_bavail = 500000;
-        buf->f_bfree  = 500000;
-        buf->f_frsize = 1024;
-    }
-    return ret;
+int statvfs(const char *path, struct statvfs *buf) {
+    static int (*o)(const char*, struct statvfs*) = NULL;
+    if (!o) o = dlsym(RTLD_NEXT, "statvfs");
+    int r = o(path, buf);
+    apply_fake_stats(buf, 1);
+    return 0;
 }
 
-int statfs(const char *path, struct statfs *buf)
-{
-    static int (*orig)(const char *, struct statfs *) = NULL;
-    if (!orig)
-        orig = dlsym(RTLD_NEXT, "statfs");
-
-    int ret = orig(path, buf);
-    if (ret == 0)
-    {
-        fakespace_hits++;
-        buf->f_blocks = 1000000;
-        buf->f_bavail = 500000;
-        buf->f_bfree  = 500000;
-        buf->f_bsize  = 1024;
-    }
-    return ret;
+int fstatfs64(int fd, struct statfs64 *buf) {
+    static int (*o)(int, struct statfs64*) = NULL;
+    if (!o) o = dlsym(RTLD_NEXT, "fstatfs64");
+    int r = o(fd, buf);
+    apply_fake_stats(buf, 0);
+    return 0;
 }
 
-int statfs64(const char *path, struct statfs64 *buf)
-{
-    static int (*orig)(const char *, struct statfs64 *) = NULL;
-    if (!orig)
-        orig = dlsym(RTLD_NEXT, "statfs64");
-
-    int ret = orig(path, buf);
-    if (ret == 0)
-    {
-        fakespace_hits++;
-        buf->f_blocks = 1000000;
-        buf->f_bavail = 500000;
-        buf->f_bfree  = 500000;
-        buf->f_bsize  = 1024;
-    }
-    return ret;
-}
-
-long syscall(long number, ...)
-{
-    static long (*orig_syscall)(long, ...) = NULL;
-    if (!orig_syscall)
-        orig_syscall = dlsym(RTLD_NEXT, "syscall");
-
-    va_list ap;
-    va_start(ap, number);
-
-    if (number == __NR_statfs || number == __NR_fstatfs)
-    {
-        const char *path = va_arg(ap, const char *);
-        struct statfs *buf = va_arg(ap, struct statfs *);
-
-        long ret = orig_syscall(number, path, buf);
-        if (ret == 0 && buf)
-        {
-            fakespace_hits++;
-            buf->f_blocks = 1000000;
-            buf->f_bavail = 500000;
-            buf->f_bfree  = 500000;
-            buf->f_bsize  = 1024;
-        }
-        va_end(ap);
-        return ret;
-    }
-
-    long a1 = va_arg(ap, long);
-    long a2 = va_arg(ap, long);
-    long a3 = va_arg(ap, long);
-    long a4 = va_arg(ap, long);
-    long a5 = va_arg(ap, long);
-    long a6 = va_arg(ap, long);
-    va_end(ap);
-
-    return orig_syscall(number, a1, a2, a3, a4, a5, a6);
-}
-
-int fstatvfs(int fd, struct statvfs *buf)
-{
-    static int (*orig)(int, struct statvfs *) = NULL;
-    if (!orig) orig = dlsym(RTLD_NEXT, "fstatvfs");
-
-    int ret = orig(fd, buf);
-    if (ret == 0)
-    {
-        fakespace_hits++;
-        buf->f_blocks = 1000000;
-        buf->f_bavail = 500000;
-        buf->f_bfree  = 500000;
-        buf->f_frsize = 1024;
-    }
-    return ret;
-}
-
-int fstatvfs64(int fd, struct statvfs64 *buf)
-{
-    static int (*orig)(int, struct statvfs64 *) = NULL;
-    if (!orig) orig = dlsym(RTLD_NEXT, "fstatvfs64");
-
-    int ret = orig(fd, buf);
-    if (ret == 0)
-    {
-        fakespace_hits++;
-        buf->f_blocks = 1000000;
-        buf->f_bavail = 500000;
-        buf->f_bfree  = 500000;
-        buf->f_frsize = 1024;
-    }
-    return ret;
+int fstatfs(int fd, struct statfs *buf) {
+    static int (*o)(int, struct statfs*) = NULL;
+    if (!o) o = dlsym(RTLD_NEXT, "fstatfs");
+    int r = o(fd, buf);
+    apply_fake_stats(buf, 0);
+    return 0;
 }
