e894331d2933e85efa0b815ff4c8c83ddd0c3a1d
Jeod <47716344+JeodC@users.noreply.github.com>
2026-05-19 21:42:54 -0400

    [PIZZATOWER] Use gmloader fmod plugin lib and runtime

--- ports/released/gamemakerengine/pizzatower/Pizza Tower.sh
+++ ports/released/gamemakerengine/pizzatower/Pizza Tower.sh
@@ -17,33 +17,42 @@ source $controlfolder/tasksetter
 [ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
 get_controls
 
-export ESUDO=$ESUDO
-export GAMEDIR="/$directory/ports/pizzatower"
+# Variables
+GAMEDIR="/$directory/ports/pizzatower"
+GMLOADER="$HOME/gmloadernext"
+GMLOADER_RUNTIME="$controlfolder/libs/gmloadernext.squashfs"
 cd "$GAMEDIR"
 
 # Log execution
 > "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
 
-# Custom gmloadernext used, no runtime
+# Mount gmloadernext runtime
+if [ -f "$GMLOADER_RUNTIME" ]; then
+    $ESUDO mkdir -p "$GMLOADER"
+    $ESUDO umount "$GMLOADER" 2>/dev/null || true
+    $ESUDO mount "$GMLOADER_RUNTIME" "$GMLOADER"
+else
+    pm_message "This port requires the gmloadernext runtime. Please download it."
+    pm_finish
+    exit 1
+fi
 
 # Exports
-export LD_LIBRARY_PATH="$GAMEDIR/libs:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="$GAMEDIR/libs:$GMLOADER/lib:$LD_LIBRARY_PATH"
 export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
-export controlfolder
-export DEVICE_ARCH
+export GMLOADER_LIB_PATH="$GMLOADER/lib"
 
 # Permissions
 $ESUDO chmod +x "$GAMEDIR/tools/patchscript"
-$ESUDO chmod +x "$GAMEDIR/gmloadernext.aarch64"
 
 # Check if we need to patch
 if [ ! -f patchlog.txt ] || [ -f "$GAMEDIR/assets/data.win" ]; then
     if [ -f "$controlfolder/utils/patcher.txt" ]; then            
-        # Setup and execute the Portmaster Patcher utility with our patch file
         export PATCHER_FILE="$GAMEDIR/tools/patchscript"
         export PATCHER_GAME="$(basename "${0%.*}")"
         export PATCHER_TIME="a while"
         export controlfolder
+        export DEVICE_ARCH
         source "$controlfolder/utils/patcher.txt"
         $ESUDO kill -9 $(pidof gptokeyb)
     else
@@ -53,27 +62,11 @@ if [ ! -f patchlog.txt ] || [ -f "$GAMEDIR/assets/data.win" ]; then
     fi
 fi
 
-
-swapabxy() {
-    # Update SDL_GAMECONTROLLERCONFIG to swap a/b and x/y button
-    PYTHON=$(command -v python3)
-    if [ "$CFW_NAME" == "knulli" ] && [ -f "$SDL_GAMECONTROLLERCONFIG_FILE" ];then
-        cat "$SDL_GAMECONTROLLERCONFIG_FILE" | $PYTHON $GAMEDIR/tools/swapabxy.py > "$GAMEDIR/gamecontrollerdb_swapped.txt"
-        export SDL_GAMECONTROLLERCONFIG_FILE="$GAMEDIR/gamecontrollerdb_swapped.txt"
-    else
-        export SDL_GAMECONTROLLERCONFIG="`echo "$SDL_GAMECONTROLLERCONFIG" | $PYTHON $GAMEDIR/tools/swapabxy.py`"
-    fi
-}
-
-# Swap a/b and x/y button if needed
-if [ -f "$GAMEDIR/swapabxy.txt" ]; then
-    swapabxy
-fi
-
 # Run
-$GPTOKEYB "gmloadernext.aarch64" -c "pizza.gptk" & 
-pm_platform_helper "$GAMEDIR/gmloadernext.aarch64" > /dev/null
-$TASKSET "$GAMEDIR/gmloadernext.aarch64" -c gmloader.json
+$GPTOKEYB "gmloadernext.aarch64" -c "pizza.gptk" &
+pm_platform_helper "$GMLOADER/gmloadernext.aarch64" > /dev/null
+$TASKSET "$GMLOADER/gmloadernext.aarch64" -c gmloader.json
 
 # Cleanup
+$ESUDO umount "$GMLOADER" 2>/dev/null || true
 pm_finish
